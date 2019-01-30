2feb3a2cfe68dc59474b054e095ee4beecfb9873
Evan Rupert <rupertevanr@gmail.com>
2019-01-29 22:27:44 -0500

    Added comments

--- lib/async_file_downloader.ex
+++ lib/async_file_downloader.ex
@@ -6,8 +6,11 @@ defmodule AsyncFileDownloader do
   @restart_wait 500
 
   def main(args \\ []) do
+    # Parse program arugments and print error if improper
     with {:ok, parameters} <- CLI.parse(args) do
+      # Read all urls from the given filename
       urls = Downloader.read_urls_from_file(parameters.filename)
+
       download_files(urls, parameters)
     else
       {:error, msg} ->
@@ -17,19 +20,25 @@ defmodule AsyncFileDownloader do
   end
 
   defp download_files(urls, parameters) do
+    # Start the thread/process to wait for the specified amount of time
+    # then alert the main process
     start_timer(parameters.timeout)
 
+    # Map over the list of urls starting a download process for each one
     urls
     |> Enum.each(fn url ->
       start_download_process(url, parameters.destination)
     end)
 
+    # Wait for respones from the processes
     watch_for_responses()
   end
 
   defp start_timer(timeout) do
+    # Save the pid of the current process to be used later
     home = self()
 
+    # Spawn a process that will sleep then send a ':stop' message to the main process
     spawn(fn ->
       Process.sleep(timeout * 1000)
       send(home, :stop)
@@ -37,26 +46,35 @@ defmodule AsyncFileDownloader do
   end
 
   defp start_download_process(url, destination) do
+    # Save the pid of the current process to be used later
     home = self()
 
+    # Start a process to download the file then send the result back to the main process
     spawn(fn ->
       Logger.debug "Started process to download: #{url}"
       result = Downloader.download_file(url, destination)
 
+      # Sent result back home
       send(home, result)
     end)
   end
 
   defp watch_for_responses do
     receive do
+      # If received the stop message from the timer process
+      # then print a log and kill the whole application
       :stop ->
         Logger.info("Recieved kill cmd from timer... ending process...")
         System.stop()
 
+      # If received an :ok and a filename
+      # then print a log and start watching again
       {:ok, filename} ->
         Logger.info("Successfuly downloaded file: '#{filename}'")
         watch_for_responses()
 
+      # If recieved an error message
+      # then print logs and restart the individual download process and start watching again
       {:error, msg, url, destination} ->
         Logger.error("Recieved error: '#{inspect msg}' when attempting download of: '#{url}'")
         Logger.error("Waiting #{@restart_wait} milliseconds and restarting")
--- lib/async_file_downloader/downloader.ex
+++ lib/async_file_downloader/downloader.ex
@@ -1,8 +1,10 @@
 defmodule AsyncFileDownloader.Downloader do
   def download_file(url, destination) do
+    # Download content from the url
     with {:ok, resp} <- HTTPoison.get(url) do
       write_resp_to_file(resp, url, destination)
     else
+      # If there is an error getting the url then return an error with additional information
       {:error, msg} ->
         {:error, msg, url, destination}
     end
@@ -18,9 +20,11 @@ defmodule AsyncFileDownloader.Downloader do
     filename = Path.basename(url)
     dest = Path.join(destination, filename)
 
+    # Attempt to write the response to the given destination
     with :ok <- File.write(dest, resp.body) do
       {:ok, filename}
     else
+      # If there is an error writting to the file, then return an error with additional information
       {:error, msg} ->
         {:error, msg, url, destination}
     end
